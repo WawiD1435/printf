b01bc260c495d71b11eb76d3773d4c881b88776b
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-05-17 08:27:31 -0700

    updated printf

--- get_print_func.c
+++ get_print_func.c
@@ -11,6 +11,8 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 		{"c", print_chr}, {"s", print_str},
 		{" %", print_prg}, {"i", print_int},
 		{"d", print_int}, {"b", print_bnr},
+		{"u", print_unt}, {"o",print_oct},
+		{"x", print_hex}, {"X", print_upx},
 		{NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
