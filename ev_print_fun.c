e662e2b47fc03e09b052dbe649b6aaa314b3cbb9
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-05-17 09:36:32 -0700

    updated printf

--- ev_print_fun.c
+++ ev_print_fun.c
@@ -10,7 +10,7 @@ int ev_print_func(const char *s, int index)
 {
 	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str}, {"i", print_int},
-		{"d", print_int}, {" %", print_prg}, {"b", print_bnr},
+		{"d", print_int}, {"%", print_prg}, {"b", print_bnr},
 		{"u", print_unt}, {"o", print_oct}, {"x", print_hex},
 		{"X", print_upx}, {"S" , print_usr}, {"p", print_add},
 		{NULL, NULL},
