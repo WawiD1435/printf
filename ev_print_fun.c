757b5a979ba96fa8265d25549f1bcd1a2da6b328
kinfeurael <kinfeuraelwubishet@gmail.com>
2022-05-17 19:16:59 -0700

    updated printf

--- ev_print_fun.c
+++ ev_print_fun.c
@@ -19,7 +19,8 @@ int ev_print_func(const char *s, int index)
 		{"+o", print_oct}, {"+x", print_hex}, {"+X", print_upx},
 		{" i", prinsint}, {" d", prinsint}, {" u", print_unt},
 		{" o", print_oct}, {" x", print_hex}, {" X", print_upx},
-		{NULL, NULL},
+		{" +i", prinpint}, {" +d", prinpint}, {"+ i", prinpint},
+		{"+ d", prinpint}, {" %", print_prg}, {NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
 
